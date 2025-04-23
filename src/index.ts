1b7701de455de835b5adec4b4ce6f932dea03276
dayoung <kimdayoung771@gmail.com>
2025-04-23 17:09:58 +1200

    feat: absolute path of db

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+import * as dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+dotenv.config();
+
+const rawUrl = process.env.DATABASE_URL!;
+if (rawUrl.startsWith('file:')) {
+  const relativePath = rawUrl.replace(/^file:/, '');
+  const absolutePath = path.resolve(process.cwd(), relativePath);
+  process.env.DATABASE_URL = `file:${absolutePath}`;
+}
+
+import { PrismaClient } from '@prisma/client';
+export const prisma = new PrismaClient();
+
+
 import express from 'express'
 import booksRouter from './router/booksRouter'
 import { errorHandler } from './middleware/errorHandler'
@@ -21,3 +37,4 @@ app.use(errorHandler)
 app.listen(port, () => {
     console.log(`🚀 Example app listening at http://localhost:${port}`)
 })
+
